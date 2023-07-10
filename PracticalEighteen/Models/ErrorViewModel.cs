8216221af315984e6ed1cd191072854a5ea16ca4
Vipul1432 <vipul.v@simformsolutions.com>
2023-07-10 15:21:59 +0530

    Add project files.

---
+++ PracticalEighteen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticalEighteen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
